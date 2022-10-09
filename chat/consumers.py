a721e88f2f4f73385b3769ab2df6b98d1fe7604f
afshintrg <ataraghijoo@gmial.com>
2022-10-09 12:20:30 +0330

    Modify Consumer.py -add connect, disconnect, receive func Add room.html

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,4 +2,14 @@ from channels.generic.websocket import WebsocketConsumer
 
 
 class ChatConsumer(WebsocketConsumer):
-    pass
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['username']
+        self.room_group_name = 'chat_%s' % self.room_name
+        self.accept()
+
+    def disconnect(self, close_code):
+        pass
+
+    # Receive message from WebSocket
+    def receive(self, text_data):
+        pass
